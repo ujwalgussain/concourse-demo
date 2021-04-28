94f89bdbbfee63e80e38b6bc046b6f929f85e3b2
ujwal.gussain@hashedin.com <ujwal.gussain@hashedin.com>
2021-04-28 16:31:02 +0530

    changes

---
+++ src/main/java/com/concourse/demo/consoursedemo/ConsourseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.concourse.demo.consoursedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsourseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsourseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concourse/demo/consoursedemo/ConsourseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concourse.demo.consoursedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsourseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
